cf3f920f79294cdbc33f33f9cea8d7aa95d10446
bitcoin <782553956@qq.com>
2022-09-29 15:34:11 +0800

    feat(fix): fix

--- craco.config.js
+++ craco.config.js
@@ -42,84 +42,112 @@ module.exports = {
         config.optimization = {
           splitChunks: {
             chunks: 'all',
-            minSize: 3000,
+            minSize: 20000,
             minChunks: 1,
-            maxAsyncRequests: 5,
-            maxInitialRequests: 6,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
             automaticNameDelimiter: '-',
             name: true,
             cacheGroups: {
-              bn: {
-                name: 'bn.js',
-                test: /[\\/]node_modules[\\/][a-zA-Z]*bn.js[\\/]/,
-                priority: 100,
-              },
-              bignumber: {
-                name: 'bignumber.js',
-                test: /[\\/]node_modules[\\/]bignumber.js[\\/]/,
-                priority: 100,
-              },
-              styledcomponent: {
-                name: 'styled-component',
-                test: /[\\/]node_modules[\\/]styled-component[\\/]/,
-                priority: 60,
-              },
-              lodash: {
-                name: 'lodash',
-                test: /[\\/]node_modules[\\/]lodash[\\/]/,
-                priority: 80,
-              },
-              moment: {
-                name: 'moment',
-                test: /[\\/]node_modules[\\/]moment[\\/]/,
-                priority: 80,
-              },
-              reactdom: {
-                name: 'react-dom',
-                test: /[\\/]node_modules[\\/]react-dom[\\/]/,
-                priority: 80,
-              },
-              reactrouterdom: {
-                name: 'react-router-dom',
-                test: /[\\/]node_modules[\\/]react-router-dom[\\/]/,
-                priority: 60,
-              },
-              web3: {
-                name: 'web3',
-                test: /[\\/]node_modules[\\/]web3[\\/]/,
-                priority: 40,
-              },
-              ethersproject: {
-                name: 'ethersproject',
-                test: /[\\/]node_modules[\\/]\@ethersproject[\\/]/,
-                priority: 40,
+              elliptic: {
+                name: 'elliptic.js',
+                test: /[\\/]node_modules[\\/][a-zA-Z]*elliptic.js[\\/]/,
+                priority: 1,
               },
               ethereumjs: {
                 name: 'ethereumjs',
-                test: /[\\/]node_modules[\\/]ethereumjs-[a-zA-Z]*[\\/]/,
+                test: /[\\/]node_modules[\\/]@ethereumjs[a-zA-Z]*[\\/]/,
                 priority: 40,
               },
-              antd: {
-                name: 'antd',
-                test: /[\\/]node_modules[\\/]antd[\\/]/,
-                priority: 40,
+              gnosis: {
+                name: 'gnosis',
+                test: /[\\/]node_modules[\\/]@gnosis[a-zA-Z]*[\\/]/,
+                priority: 50,
               },
-              antdesign: {
-                name: '@ant-design',
-                test: /[\\/]node_modules[\\/]@ant-design[\\/]/,
-                priority: 40,
+              ethersproject: {
+                name: 'ethersproject',
+                test: /[\\/]node_modules[\\/]@ethersproject[a-zA-Z]*[\\/]/,
+                priority: 60,
               },
-              idna: {
-                name: 'idna-uts46-hx',
-                test: /[\\/]node_modules[\\/]idna-uts46-hx[\\/]/,
-                priority: 90,
+              ethers: {
+                name: 'ethers',
+                test: /[\\/]node_modules[\\/]ethers[a-zA-Z]*[\\/]/,
+                priority: 70,
               },
-              sha3: {
-                name: 'sha3',
-                test: /[\\/]node_modules[\\/]js-sha3[\\/]/,
-                priority: 90,
-              },
-            },
+            }
+            // cacheGroups: {
+            //   bn: {
+            //     name: 'bn.js',
+            //     test: /[\\/]node_modules[\\/][a-zA-Z]*bn.js[\\/]/,
+            //     priority: 100,
+            //   },
+            //   bignumber: {
+            //     name: 'bignumber.js',
+            //     test: /[\\/]node_modules[\\/]bignumber.js[\\/]/,
+            //     priority: 100,
+            //   },
+            //   styledcomponent: {
+            //     name: 'styled-component',
+            //     test: /[\\/]node_modules[\\/]styled-component[\\/]/,
+            //     priority: 60,
+            //   },
+            //   lodash: {
+            //     name: 'lodash',
+            //     test: /[\\/]node_modules[\\/]lodash[\\/]/,
+            //     priority: 80,
+            //   },
+            //   moment: {
+            //     name: 'moment',
+            //     test: /[\\/]node_modules[\\/]moment[\\/]/,
+            //     priority: 80,
+            //   },
+            //   reactdom: {
+            //     name: 'react-dom',
+            //     test: /[\\/]node_modules[\\/]react-dom[\\/]/,
+            //     priority: 80,
+            //   },
+            //   reactrouterdom: {
+            //     name: 'react-router-dom',
+            //     test: /[\\/]node_modules[\\/]react-router-dom[\\/]/,
+            //     priority: 60,
+            //   },
+            //   web3: {
+            //     name: 'web3',
+            //     test: /[\\/]node_modules[\\/]web3[\\/]/,
+            //     priority: 40,
+            //   },
+            //   ethersproject: {
+            //     name: 'ethersproject',
+            //     test: /[\\/]node_modules[\\/]\@ethersproject[\\/]/,
+            //     priority: 40,
+            //   },
+            //   ethereumjs: {
+            //     name: 'ethereumjs',
+            //     test: /[\\/]node_modules[\\/]ethereumjs-[a-zA-Z]*[\\/]/,
+            //     priority: 40,
+            //   },
+            //   antd: {
+            //     name: 'antd',
+            //     test: /[\\/]node_modules[\\/]antd[\\/]/,
+            //     priority: 100,
+            //   },
+            //   antdesign: {
+            //     name: '@ant-design',
+            //     test: /[\\/]node_modules[\\/]@ant-design[\\/]/,
+            //     priority: 40,
+            //   },
+            //   idna: {
+            //     name: 'idna-uts46-hx',
+            //     test: /[\\/]node_modules[\\/]idna-uts46-hx[\\/]/,
+            //     priority: 90,
+            //   },
+            //   sha3: {
+            //     name: 'sha3',
+            //     test: /[\\/]node_modules[\\/]js-sha3[\\/]/,
+            //     priority: 90,
+            //   },
+            // },
           },
         }
       }
